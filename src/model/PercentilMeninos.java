3a09c7b5bc2a15dd54712198c720c59a0af5c9bc
Diego Cerqueira <xdiegocerqueira@gmail.com>
2017-04-09 10:18:04 -0300

    Percentil meninos progress

--- src/model/PercentilMeninos.java
+++ src/model/PercentilMeninos.java
@@ -57,26 +57,81 @@ public class PercentilMeninos {
 				// Obesidade = acima do percentil 95
 				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(97);
 			}
-		} else if (idade >= 19 && idade < 20) {
-			if (imc <= 19) {
+		} else if (idade >= 10 && idade < 12) {
+			if (imc <= 14.4) {
+				// Baixo peso = abaixo do percentil 10.
 				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(3);
-			} else if (imc > 19 && imc < 20) {
-				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(5);
-			} else if (imc > 20 && imc < 21) {
-				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(25);
-			} else if (imc > 21 && imc < 23) {
+			} else if (imc > 14.4 && imc < 19.1) {
+				// Peso normal = entre o percentil 15 e 85
 				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(50);
-			} else if (imc > 23 && imc < 25.2) {
-				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(85);
-			} else if (imc > 25.2 && imc < 27) {
-				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(90);
-			} else if (imc > 27 && imc < 28.1) {
-				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(95);
-			} else if (imc > 28.1) {
+			} else if (imc > 19.1 && imc < 22.1) {
+				// Sobrepeso = entre o percentil 85 e 95
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(86);
+			} else if (imc > 22.1) {
+				// Obesidade = acima do percentil 95
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(97);
+			}
+
+		} else if (idade >= 12 && idade < 14) {
+			if (imc <= 15.2) {
+				// Baixo peso = abaixo do percentil 10.
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(3);
+			} else if (imc > 15.2 && imc < 21.3) {
+				// Peso normal = entre o percentil 15 e 85
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(50);
+			} else if (imc > 21.3 && imc < 25.2) {
+				// Sobrepeso = entre o percentil 85 e 95
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(86);
+			} else if (imc > 25.2) {
+				// Obesidade = acima do percentil 95
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(97);
+			}
+
+		} else if (idade >= 14 && idade < 16) {
+			if (imc <= 16.5) {
+				// Baixo peso = abaixo do percentil 10.
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(3);
+			} else if (imc > 16.5 && imc < 22.5) {
+				// Peso normal = entre o percentil 15 e 85
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(50);
+			} else if (imc > 22.5 && imc < 26.1) {
+				// Sobrepeso = entre o percentil 85 e 95
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(86);
+			} else if (imc > 26.1) {
+				// Obesidade = acima do percentil 95
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(97);
+			}
+		} else if (idade >= 16 && idade < 18) {
+			if (imc <= 18) {
+				// Baixo peso = abaixo do percentil 10.
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(3);
+			} else if (imc > 18 && imc < 24.2) {
+				// Peso normal = entre o percentil 15 e 85
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(50);
+			} else if (imc > 24.2 && imc < 27.4) {
+				// Sobrepeso = entre o percentil 85 e 95
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(86);
+			} else if (imc > 27.4) {
+				// Obesidade = acima do percentil 95
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(97);
+			}
+
+		} else if (idade >= 18 && idade < 20) {
+			if (imc < 19) {
+				// Baixo peso = abaixo do percentil 10.
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(3);
+			} else if (imc >19 & imc < 26) {
+				// Peso normal = entre o percentil 15 e 85
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(50);
+			} else if (imc > 26 && imc < 29.3) {
+				// Sobrepeso = entre o percentil 85 e 95
+				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(86);
+			} else if (imc > 29.3) {
+				// Obesidade = acima do percentil 95
 				return RecebePercentilRetornaPeso.recebePercentilRetornaPeso(97);
 			}
 
 		}
-		return null;
+		throw new IllegalAccessError("");
 	}
 }
